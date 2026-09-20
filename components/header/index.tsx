8f277ef20f40dac19a235df70b73afb2953f1ab6
Jonathan Pereira <jonathanbrancopereira@gmail.com>
2026-09-20 21:07:34 +0100

    feat: update header component with search button and improve layout; refactor root layout to use Tabs

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@/components/ui/text'
+import { Search } from 'lucide-react-native'
 import { Box } from '../ui/box'
 import { Button } from '../ui/button'
 import { HStack } from '../ui/hstack'
@@ -7,11 +8,15 @@ const Header = () => {
     return (
         <HStack space="md" className="justify-between items-center">
             <Box>
-                <Text>Header</Text>
-                <Text>Good morning, Jonathan</Text>
+                <Text className="text-base font-light">Header</Text>
+                <Text className="text-lg font-bold">
+                    Good morning, Jonathan
+                </Text>
             </Box>
             <Box>
-                <Button variant="ghost"></Button>
+                <Button className="w-9 h-9 rounded-lg" variant="secondary">
+                    <Search className="h-3 w-3" />
+                </Button>
             </Box>
         </HStack>
     )
--- src/app/(root)/_layout.tsx
+++ src/app/(root)/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from 'expo-router'
+import { Tabs } from 'expo-router'
 
 export default function RootLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+            }}
+        />
+    )
 }
